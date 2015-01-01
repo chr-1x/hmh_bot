6feacf41c3078000f38084f7600a558edc2bdbc2
abnercoimbre <abner.coimbre@upr.edu>
2015-01-01 16:09:16 -0500

    Updated wrench command.

--- modules/handmade_faq.py
+++ modules/handmade_faq.py
@@ -189,5 +189,5 @@ def whatcommand(bot, trigger):
     info(bot, trigger, "This is a stream about learning to code games from scratch. For more information, see !game, !stream, !who")
 
 @command("wrench")
-def codeIsAToll(bot, trigger):
-    info(bot, trigger, "Programming is not about code. Code is the tool used to solve the problems programmers must addressed. For more on this viewpoint, watch Mike Acton's talk: 'Data-Oriented Design and C++' (https://www.youtube.com/watch?v=rX0ItVEVjHc)"
+def codeIsATool(bot, trigger):
+    info(bot, trigger, "Programming is not about the languages. Code is the tool used to solve the problems programmers must address. For more on this perspective, refer to Mike Acton's talk: 'Data-Oriented Design and C++' (https://www.youtube.com/watch?v=rX0ItVEVjHc)"
