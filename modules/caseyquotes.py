0da7879d109b1158c7d2d11e94c811d4b4c93fb2
Kurt Kartaltepe <kkartalepe@gmail.com>
2015-02-09 01:28:35 -0600

    Add quote management
    Fixes Chronister/ChronalRobot#30
    Fixes Chronister/ChronalRobot#31
    Adds getting a specfic quote
    Adds quote# when stating quotes

--- modules/caseyquotes.py
+++ modules/caseyquotes.py
@@ -10,6 +10,7 @@ sys.path.append(os.path.dirname(__file__))
 
 from handmade import command, adminonly, whitelisted_streamtime
 
+dateParser = parsedatetime.Calendar()
 dbURI = 'sqlite:/:memory:'
 defaultTz = 'US/Pacific'
 
@@ -28,10 +29,17 @@ class Quote(SQLObject):
 		self.timestamp = newStart.to('UTC').timestamp
 
 def setup(bot):
-	global dbURI 
+	global dbURI
 	dbURI = bot.config.db.userdb_type+'://'+os.path.abspath(bot.config.db.userdb_file) # URI must be absolute.
 	requireDb()
 	Quote.createTable(ifNotExists=True)
+
+def getQuote(quoteId):
+	quoteId = int(quoteId)
+	try:
+		return Quote.get(quoteId);
+	except SQLObjectNotFound:
+		return None
 
 @adminonly
 @command("addquote", "aq")
@@ -39,8 +47,62 @@ def addQuote(bot, trigger):
 	requireDb()
 	text = trigger.group(2)
 	# Perhaps someone would want to set the time for a quote.
-	Quote(text=text, timestamp=arrow.now().timestamp)
-	bot.say("Quote added!")
+	newQuote = Quote(text=text, timestamp=arrow.now().timestamp)
+	bot.say("Quote #%d added!" % (newQuote.id))
+
+@adminonly
+@command("deletequote", "dq")
+def delQuote(bot, trigger):
+	requireDb()
+	quote = getQuote(trigger.group(2))
+	if(quote != None):
+		bot.say("Could not find quote #%s" % (trigger.group(2)))
+		return
+
+	bot.say("Deleted quote %i '%s'" % (quote.id, quote.text))
+	quote.destroySelf()
+
+@adminonly
+@command("fixquote", "fq")
+def fixQuote(bot, trigger):
+	requireDb()
+	split = trigger.group(2).split(' ', 1);
+	if(len(split) != 2):
+		bot.say("Please provide the fixed quote!")
+		return
+
+	quote = getQuote(split[0])
+	if(quote == None):
+		bot.say("Could not find quote #%s" % (split[0]))
+		return
+
+	quote.text = split[1]
+	bot.say('Quote #%d fixed to "%s"' % (quote.id, quote.text))
+
+@adminonly
+@command("fixquotetime", "fqt")
+def fixQuoteTime(bot, trigger):
+	requireDb()
+	split = trigger.group(2).split(' ', 1);
+	if(len(split) != 2):
+		bot.say("Please provide the fixed quote!")
+		return
+
+	quote = getQuote(split[0])
+	if(quote == None):
+		bot.say("Could not find quote #%s" % (split[0]))
+		return
+
+	pTime,flag = dateParser.parseDT(split[1], sourceTime=arrow.now(defaultTz)) # use beginning of today as the source day to ensure DT returned.
+	#Anything not explicitly dated will go to next occurance of that date
+	#so check if the date given was pushed more than 6 months in the future
+	#if it was assume the issuer ment the past. Will break setting dates manually with years.
+	pTime = arrow.get(pTime, defaultTz) # avoid python AWFUL datetimes.
+	if (pTime > arrow.now(defaultTz).replace(months=+6)):
+			pTime = pTime.replace(years=-1)
+
+	quote.timestamp = pTime.to('UTC').timestamp;
+	bot.say("Quote #%d moved to date: %s" %(quote.id, quote.time.strftime("%b %d")))
 
 @whitelisted_streamtime
 @command("randomquote", "rq")
@@ -52,5 +114,18 @@ def randomQuote(bot, trigger):
 		return
 	randomNum = random.randrange(numQuotes)
 	quote = query[randomNum]
-	bot.say('"%s" -Casey %s' % (quote.text, quote.time.strftime("%b %d")))
+	bot.say('(#%d)"%s" -Casey %s' % (quote.id, quote.text, quote.time.strftime("%b %d")))
+
+@whitelisted_streamtime
+@command("sayquote", "quote")
+def sayQuote(bot, trigger):
+	args = trigger.group(2);
+	if(args):
+		quote = getQuote(args)
+		if(quote == None):
+			bot.say("No such quote found!")
+		else:
+			bot.say('(#%d)"%s" -Casey %s' % (quote.id, quote.text, quote.time.strftime("%b %d")))
+	else:
+		randomQuote(bot, trigger)
 
