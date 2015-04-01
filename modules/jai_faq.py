fa10e094d84891580093b6e8d9477afa399bb4e2
abnercoimbre <abner.coimbre@upr.edu>
2015-03-31 23:12:23 -0400

    Minor updates to existing commands.

--- modules/jai_faq.py
+++ modules/jai_faq.py
@@ -5,11 +5,11 @@ from streambot import Cmd, command, info, whitelisted, adminonly, whitelisted_st
 
 @command("jai", "what", "lang")
 def jai(bot, trigger):
-	info(bot, trigger, "JAI is the codename for the language Jonathan Blow is creating to address common problems game programmers face using existing languages. To more about these problems in detail, check out http://goo.gl/dZ9QVe")
+	info(bot, trigger, "JAI is the codename for the language Jonathan Blow is creating to address common problems game programmers face using existing languages. To learn more about these problems in detail, check out http://goo.gl/dZ9QVe")
 
 @command("jon", "who")
 def jonbio(bot, trigger):
-	info(bot, trigger, "Jonathan Blow is a game designer and programmer who released Braid (http://braid-game.com/) and is currently working on The Witness (http://the-witness.net/news) and a new programming language for games called JAI. For more info: !jai")
+	info(bot, trigger, "Jonathan Blow is a game designer and programmer, previously of Braid (http://braid-game.com/), currently working on The Witness (http://the-witness.net/news) and a new programming language for games called JAI. For more info: !jai")
 
 @command("!old", "!archive")
 def archive(bot, trigger):
@@ -19,11 +19,11 @@ def archive(bot, trigger):
 def ideInfo(bot, trigger):
     """Info command that provides information about the editor (emacs) used by Casey.
     """
-    info(bot, trigger, "Jon uses Emacs to edit his code, because that is what he is used to. There are a lot of editors out there, however, so you should use whatever you feel most comfortable in. The version he uses is GNU Emacs 23.4.1 (i386-mingw-nt6.1.7601), released in 2012.")
+    info(bot, trigger, "Jon uses Emacs to edit his code, because that is what he is used to. There are a lot of editors out there, however, so you should use whatever you feel most comfortable with.")
 
 @command('college', 'school')
 def collegeInfo(bot, trigger):
-    info(bot, trigger, "Jon did not finish college; he has been coding in the gaming industry since god knows when. You can read his thoughts here: http://number-none.com/blow")
+    info(bot, trigger, "Jon did not finish college; he has been coding professionally in the gaming industry since 1996. You can read his thoughts here: http://number-none.com/blow")
 
 @command('site')
 def siteInfo(bot, trigger):
